ae1f1d10f374a2a9f8640eb74669bea2d239f387
Daniel Hahler <git@thequod.de>
2019-08-05 18:00:19 +0200

    fix: derive from TestCase

--- mixin.py
+++ mixin.py
@@ -1,3 +1,4 @@
 class MyMixin:
+    @classmethod
     def setUpClass(cls):
         print('MyMixin.setUpClass', cls)
--- mod1/__init__.py
+++ mod1/__init__.py
@@ -1,6 +1,8 @@
+from unittest import TestCase
+
 from mixin import MyMixin
 
 
-class FirstTestCase(MyMixin):
+class FirstTestCase(MyMixin, TestCase):
     def test_1st(self):
         print("test_1st", self)
